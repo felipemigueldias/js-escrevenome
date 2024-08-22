cc11068db324b175491b229d4b9659aa36ffbb8f
felipemigueldias <miguel.dias.felipe@escola.pr.gov.br>
2024-08-22 11:21:34 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
+let cor;
 function setup() {
-  createCanvas(1900, 1000);
+  createCanvas(1400, 1000);
   background("220");
+  cor = color("black");
 }
 
 function draw() {
   
   
-  stroke("black");
-  fill("black");
+  stroke(cor);
+  fill(cor);
   
+   if(keyIsPressed){ 
+  cor = color(random(0,255),random(0,255),random(0,255));}
   //console.log(mouseIsPressed);
   if(mouseIsPressed){
       rect(mouseX,mouseY,20,30);
